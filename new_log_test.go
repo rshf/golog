16ef2c32693dc794796c6ae176d6c5fdf4b56f7a
cander <cander@hyahm.com>
2022-03-15 18:28:51 +0800

    多参数中间使用空格分割

--- new_log_test.go
+++ new_log_test.go
@@ -1,10 +1,17 @@
 package golog
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewLog(t *testing.T) {
 	defer Sync()
-	logging := NewLog("log\\test_0.log", 10<<10, false)
+	Info(1111)
+	InitLogger("log\\test.log", 0, false)
+	Info(2222)
+	logging1 := NewLog("log\\test_0.log", 10<<10, false)
+	logging2 := NewLog("log\\test_0.log", 10<<10, false)
 
-	logging.Info("44444444")
+	logging1.Info("44444444")
+	logging2.Info("5555")
 }
