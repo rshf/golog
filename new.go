efcd03a092a25cea9d821ac4b7338deb52f8977f
cander <cander_liubiao@126.com>
2022-03-31 16:01:35 +0800

    newlog clean

--- new.go
+++ new.go
@@ -34,7 +34,7 @@ type Log struct {
 func (l *Log) clean(ctx context.Context) {
 	for {
 		select {
-		case <-time.After(time.Duration(cleanTime) * time.Hour * 24):
+		case <-time.After(time.Duration(l.Expire) * time.Hour * 24):
 			fs, err := ioutil.ReadDir(l.Dir)
 			if err != nil {
 				continue
